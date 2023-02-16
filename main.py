23a2e7c31d9e22cfde0a502ede87309f7e0da467
upasanasaha <upasanasaha40@gmail.com>
2023-02-17 03:27:14 +0530

    Model trainer completed.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from predictor.entity import config_entity
 from predictor.components.data_ingestion import DataIngestion
 from predictor.components.data_validation import DataValidation
 from predictor.components.data_transformation import DataTransformation
-# from predictor.components.model_trainer import ModelTrainer
+from predictor.components.model_trainer import ModelTrainer
 # from predictor.components.model_evaluation import ModelEvaluation
 # from predictor.components.model_pusher import ModelPusher
 
@@ -31,6 +31,12 @@ if __name__=="__main__":
         data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
                 data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact= data_transformation.initiate_data_transformation()
+
+        #model trainer
+        model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, 
+                data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
 
     except Exception as e:
         raise SensorException(e,sys)
